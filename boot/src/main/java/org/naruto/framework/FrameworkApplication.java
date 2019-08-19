d9ac55f85d1370a9c6e6391c9fc0a8f4ec1f3388
liuhy <25436136@qq.com>
2019-08-19 12:03:24 +0800

    refactor

--- boot/src/main/java/org/naruto/framework/FrameworkApplication.java
+++ boot/src/main/java/org/naruto/framework/FrameworkApplication.java
@@ -1,25 +1,17 @@
 package org.naruto.framework;
 
-import org.modelmapper.ModelMapper;
 import org.naruto.framework.core.repository.CustomRepositoryFactoryBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories(repositoryFactoryBeanClass = CustomRepositoryFactoryBean.class)
-@ComponentScan(excludeFilters={@ComponentScan.Filter(type= FilterType.ANNOTATION,classes={SpringBootApplication.class})})
 public class FrameworkApplication {
 
 	public static void main(String[] args) {
