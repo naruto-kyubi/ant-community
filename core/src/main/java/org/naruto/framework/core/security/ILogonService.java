87c5bdcefacead42692a757bc7bfc45b6725dc24
qteamcheng <64435716@qq.com>
2019-08-22 10:01:16 +0800

    refactor user module

--- core/src/main/java/org/naruto/framework/core/security/ILogonService.java
+++ core/src/main/java/org/naruto/framework/core/security/ILogonService.java
@@ -1,12 +1,12 @@
 package org.naruto.framework.core.security;
 
-import org.naruto.framework.user.domain.ThirdPartyUser;
-import org.naruto.framework.user.domain.User;
+
+import org.naruto.framework.core.user.User;
 
 public interface ILogonService {
 
     User authenticate(LogonUser logonUser);
-    ThirdPartyUser bind(User user, String bindType, String bindUid, String bindName);
-    ThirdPartyUser bind(User user, String authType, String authCode);
-    void unbind(User user, String authType);
+//    ThirdPartyUser bind(User user, String bindType, String bindUid, String bindName);
+//    ThirdPartyUser bind(User user, String authType, String authCode);
+//    void unbind(User user, String authType);
 }
