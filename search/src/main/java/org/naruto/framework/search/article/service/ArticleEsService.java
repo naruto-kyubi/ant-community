486ef55e07c943c4d2a1795eb3217f81202916ae
liuhy <25436136@qq.com>
2019-08-28 12:25:38 +0800

    new add test module.

--- search/src/main/java/org/naruto/framework/search/article/service/ArticleEsService.java
+++ search/src/main/java/org/naruto/framework/search/article/service/ArticleEsService.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import java.util.Map;
 
 public interface ArticleEsService {
-    public Page<ArticleVo> search(Map map);
+    Page<ArticleVo> search(Map map);
 
     EsArticle save(EsArticle esArticle);
 
---
+++ test/src/test/java/org/naruto/framework/common/catalog/service/CatalogServiceTest.java
@@ -0,0 +1,28 @@
+package org.naruto.framework.common.catalog.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.naruto.framework.FrameworkApplication;
+import org.naruto.framework.common.catalog.domain.Catalog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FrameworkApplication.class)
+public class CatalogServiceTest {
+
+    @Autowired
+    private CatalogService catalogService;
+
+    @Test
+    public void testQueryPage(){
+        Map map = new HashMap();
+        Page<Catalog> page = catalogService.queryPage(map);
+        System.out.println("getTotalElements=" + page.getTotalElements());
+    }
+}
