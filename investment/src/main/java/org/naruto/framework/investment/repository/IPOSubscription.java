d6b682ecb4cd56fe52b49f0fb90e9f1b5466ba82
liuhy <25436136@qq.com>
2021-02-04 19:42:39 +0800

    更新中签数量（numberOfSigned）默认值为-1

--- investment/src/main/java/org/naruto/framework/investment/repository/IPOSubscription.java
+++ investment/src/main/java/org/naruto/framework/investment/repository/IPOSubscription.java
@@ -61,7 +61,7 @@ public class IPOSubscription {
     private Integer planIPO = 0;
 
     //中签数量
-    private Integer numberOfSigned = 0;
+    private Integer numberOfSigned = -1;
     
     @Transient
     private Date lastOperationAt;
