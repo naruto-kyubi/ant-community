f7f2bc67e456c7c4f9ff70bfa4028b7eb867ddd9
qteamcheng <64435716@qq.com>
2021-02-09 17:59:15 +0800

    艾德验证代码

--- investment/src/main/java/org/naruto/framework/investment/operationImpl/AideOperation.java
+++ investment/src/main/java/org/naruto/framework/investment/operationImpl/AideOperation.java
@@ -42,86 +42,6 @@ public class AideOperation extends BaseOperation {
         return text;
     }
 
-
-    public void buyNewStock(AndroidDriver<MobileElement> driver, String mobileId, String logonPageURL, String account, String pwd, String stockPageURL, String pinCodePwd) throws InterruptedException,Exception {
-
-        //打开登录页
-        driver.get(logonPageURL);
-
-        Thread.sleep(2000);
-        //输入账号、密码；
-        driver.findElementByXPath("//input[@name='user']").sendKeys(account);
-        driver.findElementByXPath("//input[@name='password']").sendKeys(pwd);
-
-        //点击登录；
-        MobileElement element1 = driver.findElementByXPath("//input[@id='btnLogin']");
-        driver.executeScript("arguments[0].click();",element1);
-
-        Thread.sleep(2000);
-//        driver.get(" https://fsec.ayers.com.hk/mts.web/Web2/front/SecondPassword.aspx");
-
-        //启动密码器
-        String pincode = getPinCode(mobileId,pinCodePwd);
-        //激活chrome浏览器程序；
-        driver.activateApp("com.android.chrome");
-
-        //切换到iframe；
-        driver.switchTo().frame("frame1");
-        //输入密码器编码；
-
-
-        driver.findElementByXPath("//input[@id='tbxESP']").sendKeys(pincode);
-
-        element1 = driver.findElementByXPath("//input[@id='btnSubmit_ESP']");
-        driver.executeScript("arguments[0].click();",element1);
-
-        //返回主页面；
-        driver.switchTo().parentFrame();
-
-        Thread.sleep(2000);
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        //同意
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='btnCloseLoginMsg']"))).click();
-        //停顿30秒，用来输入安全码；
-//        Thread.sleep(30000);
-
-        //跳转到打新股界面；
-        driver.get(stockPageURL);
-
-        while(true){
-            try{
-                //停顿1秒钟，等待页面加载完成；
-                Thread.sleep(1000);
-                //选择90%孖展，默认下拉列表第二项；
-                element1 = driver.findElementByXPath("//select[@name='loan_type']");
-                driver.executeScript("arguments[0][1].selected=true;",element1);
-
-                //点击'申请'按钮；
-                driver.findElementByXPath("//input[@name='btnApply']").click();
-
-                Thread.sleep(200);
-                //确认申购；与'申请'按钮命名一样；
-                driver.findElementByXPath("//input[@name='btnApply']").click();
-
-                Thread.sleep(1000);
-                element1 = driver.findElementByXPath("//div[@id='normalLayout1']/img");
-                String error = element1.getAttribute("src");
-
-                if (null!=error && error.contains("/mts.web/images/error.png")) {
-                    //出现错误,返回上一页面；
-                    driver.navigate().back();
-                }else{
-                    //有可能已经成功了；
-                }
-
-            }catch(Exception e){
-                e.printStackTrace();
-                //找不到，融资列表时，会抛出异常；刷新当前页面；
-                driver.navigate().refresh();
-            }
-        }
-    }
-
     @Override
     public void connect(Account account) throws Exception {
         AndroidDriver<MobileElement> driver = webSessionManager.getConnection(account.getAppLocation());
@@ -189,7 +109,7 @@ public class AideOperation extends BaseOperation {
 
     @Override
     public void prepareFinanceIPO(IPOSubscription ipoSubscription, Stock stock) throws Exception {
-        String shares = String.valueOf(ipoSubscription.getNumberOfShares());
+        String shares = String.valueOf(ipoSubscription.getPlanSubscriptionShares());
 
         log.info("The share is ...".concat(shares));
 
